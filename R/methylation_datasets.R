7bd50692993183093ddacd87c89dc53fc326208a
Jim Java <james.java@roswellpark.org>
2016-01-26 17:46:04 +0000

    Adding RTCGA.methylation, RTCGA.miRNASeq, RTCGA.mRNA, RTCGA.PANCAN12, RTCGA.RPPA, RTCGA.CNV
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/RTCGA.methylation@3581 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/methylation_datasets.R
@@ -0,0 +1,115 @@
+#' 
+#' @title Methylation datasets from TCGA project
+#'
+#' @description
+#' Package provides methylation (humanmethylation27) datasets from The Cancer Genome Atlas Project for
+#' all cohorts types from \url{http://gdac.broadinstitute.org/}.
+#' Data were downloaded using \link{RTCGA-package} and contain snapshots for
+#' the date: \code{2015-11-01}. The process is described here:
+#' \url{http://rtcga.github.io/RTCGA/}.
+#' Use cases, examples and information about datasets in \pkg{RTCGA.data}
+#' family can be found here: \code{browseVignettes("RTCGA")}. Methylation data format
+#' is explained here \url{https://wiki.nci.nih.gov/display/TCGA/DNA+methylation}.
+#' Converting \pkg{RTCGA.methylation} datasets from \code{data.frames} to Bioconductor classes is explained here \link{convertTCGA}.
+#' 
+#' @details \code{browseVignettes("RTCGA")}
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' browseVignettes("RTCGA")
+#' }
+#'
+#' 
+#' @return Data frames with methylation data.
+#' 
+#' @import RTCGA
+#' @format NULL
+#' @source \url{http://gdac.broadinstitute.org/}
+#' 
+#' 
+#' @name methylation
+#' @rdname methylation
+#' @aliases BRCA.methylation,COAD.methylation,COADREAD.methylation,GBM.methylation,GBMLGG.methylation,KIPAN.methylation,KIRC.methylation,KIRP.methylation,LAML.methylation,LUAD.methylation,LUSC.methylation,OV.methylation,OV.methylation1,OV.methylation2,READ.methylation,STAD.methylation,STES.methylation,UCEC.methylation
+#'
+"BRCA.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"COAD.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"COADREAD.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"GBM.methylation" 
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"GBMLGG.methylation"  
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"KIPAN.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"KIRC.methylation"    
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"KIRP.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"LAML.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"LUAD.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"LUSC.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"OV.methylation2"      
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"OV.methylation1" 
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"READ.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"STAD.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"STES.methylation"
+#' @name methylation
+#' @rdname methylation
+#' @format NULL
+#' 
+"UCEC.methylation"
---
+++ README.md
@@ -0,0 +1,21 @@
+# RTCGA.methylation
+
+A part of [RTCGA](https://github.com/RTCGA) family.
+
+To install development version from GitHub use
+
+````{R}
+library(RTCGA)
+installTCGA("RTCGA.methylation")
+````
+
+Make sure you have [Rtools](https://cran.r-project.org/bin/windows/Rtools/) installed on your computer, if you are trying `devtools` on Windows.
+
+To install Bioconductor development version use (the same as GitHub development version) - not yet there
+
+````{R}
+BiocInstaller::useDevel() # swiches to devel branchof Bioconductor
+source("https://bioconductor.org/biocLite.R") # downloads bioClite function
+biocLite("RTCGA.methylation") # installs a package
+````
+
